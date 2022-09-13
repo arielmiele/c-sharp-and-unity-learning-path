f415398f79e47b5aa73e4841b875ae960ca74497
Ariel Miele <miele.ariel@outlook.com>
2022-09-12 21:56:17 -0300

    New commit adding an exercise using Switch statements!

---
+++ Excercise2_Switch_Statements/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Excercise2_Switch_Statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Shows the Game Menu
+            Console.WriteLine("**************\n");
+            Console.WriteLine("Menu: \n");
+            Console.WriteLine("1 - New Game\n");
+            Console.WriteLine("2 - Load Game\n");
+            Console.WriteLine("3 - Options\n");
+            Console.WriteLine("4 - Quit\n");
+            Console.WriteLine("**************\n");
+
+            //Asks the user to enter the option
+            Console.WriteLine("Option: ");
+            int userOption = int.Parse(Console.ReadLine());
+
+            //Shows the correct message depending on the user choice
+            switch (userOption) {
+                case 1:
+                    Console.WriteLine("Starting game ...\n");
+                    break;
+                case 2:
+                    Console.WriteLine("Loading game ...\n");
+                    break;
+                case 3:
+                    Console.WriteLine("Options\n");
+                    break;
+                case 4:
+                    Console.WriteLine("Quitting game ...\n");
+                    break;
+                default:
+                    Console.WriteLine("Not a correct option!\n");
+                    break;
+            }
+            
+            //Next line is used to show the result to the user
+            Console.ReadLine();
+        }
+    }
+}
